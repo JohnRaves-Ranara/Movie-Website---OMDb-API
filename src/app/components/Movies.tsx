69d9cbb153619583f2c048f031c8c96be1d9c4af
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-04-16 18:51:10 +0800

    feat: added searchbar header

---
+++ src/app/components/Movies.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Movies() {
+    return (
+        <div className="min-h-screen bg-gray-950 flex justify-center items-center text-white">
+            <p>Search for a movie by typing in the search bar</p>
+        </div>
+    )
+}
