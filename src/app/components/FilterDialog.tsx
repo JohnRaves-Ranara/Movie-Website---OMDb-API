6480d04a2e8f4e58bee68bbf001a351aa9582fd1
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-04-28 00:26:23 +0800

    feat: genre filters are now added to url when clicking confirm
    In this commit I got rid of SelectedFiltersContext and used a local state to store all filters in
    filter dialog. I added a confirm button in filter dialogs so that when you select filters and click
    confirm, all filters will be added to with_genres= in url and if no filters are selected, with_genres
    will be removed in url.

--- src/app/components/FilterDialog.tsx
+++ src/app/components/FilterDialog.tsx
@@ -1,36 +1,112 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
+  DialogFooter,
 } from "@/components/ui/dialog";
-import { IconFilterFilled } from "@tabler/icons-react";
 import { Genre } from "../utils/types";
 import GenreFilter from "./GenreFilter";
 import { useFilterDialogContext } from "../contexts/FilterDialogContext";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 type FilterDialogProps = {
-    allGenres : Genre[]
-}
+  allGenres: Genre[];
+};
 
-export default function FilterDialog({allGenres} : FilterDialogProps ) {
-  const {isOpen, setIsOpen} = useFilterDialogContext()
+export default function FilterDialog({ allGenres }: FilterDialogProps) {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+
+  const [filters, setFilters] = useState<any[]>([]);
+  const { isOpen, setIsOpen } = useFilterDialogContext();
+
+  //get `with_genres` urlparams (if any) and convert to number array
+  const URLParamsFiltersArray = params
+    .get("with_genres")
+    ?.split(",")
+    .map((filter) => Number(filter));
+
+  useEffect(() => {
+    console.log("use effect ran");
+    //if with_genres params exists, set filters to that
+    if (URLParamsFiltersArray) {
+      setFilters(URLParamsFiltersArray);
+    }
+  }, []);
+
+  const handleSelectGenre = (genreID: number) => {
+    //remove selected filter in filters if it exists, otherwise add
+    if (filters.includes(genreID)) {
+      setFilters(filters.filter((filter) => filter !== genreID));
+    } else {
+      setFilters([...filters, genreID]);
+    }
+    console.log(filters);
+  };
+
+  function checkArrayInequality(
+    URLParamsFiltersArray: number[] | undefined,
+    filters: number[]
+  ) {
+    if (URLParamsFiltersArray) {
+      if (URLParamsFiltersArray!.length !== filters.length) return true;
+      URLParamsFiltersArray!.sort();
+      filters.sort();
+      for (let i = 0; i < URLParamsFiltersArray!.length; i++) {
+        if (URLParamsFiltersArray![i] !== filters[i]) return true;
+      }
+      return false;
+    } else {
+      return true;
+    }
+  }
 
   return (
-    <Dialog open={isOpen} onOpenChange={()=> setIsOpen(!isOpen)}>
+    <Dialog open={isOpen} onOpenChange={() => setIsOpen(false)}>
       <DialogContent className="bg-gray-950 border-gray-950">
         <DialogHeader>
           <DialogTitle className="text-muted-foreground">Select Filters</DialogTitle>
           <div className="flex flex-wrap justify-center items-center py-8 space-x-4">
             {allGenres.map((genre) => {
-                return (
-                    <GenreFilter genre={genre}></GenreFilter>
-                )
+              return (
+                <GenreFilter
+                  key={genre.id}
+                  isGenreSelected={filters.includes(genre.id)}
+                  genre={genre}
+                  handleSelectGenre={() => handleSelectGenre(genre.id)}
+                ></GenreFilter>
+              );
             })}
           </div>
         </DialogHeader>
+        <DialogFooter>
+          <button
+            onClick={() => {
+              try {
+                if (
+                  filters.length !== 0 &&
+                  checkArrayInequality(URLParamsFiltersArray, filters)
+                ) {
+                  params.set("with_genres", filters.join(","));
+                } else {
+                  params.delete("with_genres");
+                }
+              } catch (e) {
+                throw new Error(`${e}`);
+              } finally {
+                router.replace(`${pathname}?${params.toString()}`);
+                setIsOpen(false);
+              }
+            }}
+            className="bg-purple-500 py-2 px-4 text-white rounded-full"
+          >
+            Confirm
+          </button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
--- src/app/components/GenreFilter.tsx
+++ src/app/components/GenreFilter.tsx
@@ -1,26 +1,18 @@
-import { useState } from "react";
 import { Genre } from "../utils/types";
-import { useSelectedFiltersContext } from "../contexts/SelectedFiltersContext";
 
 type GenreFilterProps = {
   genre: Genre;
+  handleSelectGenre : () => void
+  isGenreSelected : boolean
 };
 
-export default function GenreFilter({ genre }: GenreFilterProps) {
-  const {filters, setFilters} = useSelectedFiltersContext()
+export default function GenreFilter({ genre, handleSelectGenre, isGenreSelected }: GenreFilterProps) {
 
   return (
     <button
-      onClick={() => {
-        if(filters.includes(genre.id)){
-          setFilters(filters.filter((filter) => filter !== genre.id))
-        }
-        else{
-          setFilters([...filters, genre.id])
-        }
-      }}
+    onClick={handleSelectGenre}
       type="button"
-      className={`rounded-full ${filters.includes(genre.id) ? "bg-purple-500 text-white" : "bg-purple-900 text-gray-400"} inline-flex items-center justify-center px-4 py-2 my-2`}
+      className={`rounded-full ${isGenreSelected ? "bg-purple-500 text-white" : "bg-purple-900 text-gray-400"} inline-flex items-center justify-center px-4 py-2 my-2`}
     >
       <p>{genre.name}</p>
     </button>
