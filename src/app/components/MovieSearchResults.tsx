337006ddfc8583579f895773eb1055610ed1a3a2
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-05-03 17:20:31 +0800

    feat: added responsiveness to the MovieSearchResults
    In this commit I:
    
    - added responsiveness to MovieSearchResults component
    - added responsiveness to Pagination component
    - used Headwind to sort tailwind classes
    - Added height-300px to MoviePoster if the image comes from MovieDetailsPage

--- src/app/components/MovieSearchResults.tsx
+++ src/app/components/MovieSearchResults.tsx
@@ -6,30 +6,25 @@ import UsePagination from "./UsePagination";
 type MovieSearchResultsProps = {
   movies: Movie[];
   inputQuery: string;
-  totalPages : number
-  allGenres : Genre[]
+  totalPages: number;
+  allGenres: Genre[];
 };
 
 export default function MovieSearchResults({
   movies,
   inputQuery,
   totalPages,
-  allGenres
+  allGenres,
 }: MovieSearchResultsProps) {
-  
-
   return (
-    <div className="min-h-screen bg-gray-950 pt-[18vh] px-24 pb-24 ">
-      <div className="flex gap-8 items-center mb-8">
-        <h1 className="text-white text-[2.5vw] overflow-hidden before:block before:h-8 before:w-[0.45rem] before:bg-purple-500 flex items-center gap-4">
+    <div className="min-h-screen bg-gray-950 px-[4vw] lg:px-24 pt-[12vh] sm:pt-[14vh] md:pt-[16vh] lg:pt-[18vh] xl:pt-[16vh] pb-24 ">
+      <div className="flex items-center gap-8 mb-8">
+      <h1 className="text-white text-md mobile-l:text-lg sm:text-2xl lg:text-3xl xl:text-4xl overflow-hidden before:block before:h-8 before:w-[0.45rem] before:bg-purple-500 flex items-center gap-4">
+        
           Showing results for "{inputQuery}"
         </h1>
-        {/* <div className="px-6 py-2 bg-purple-500 rounded-full flex items-center gap-3 hover:bg-purple-800 group">
-            <p className="text-white">Filter</p>
-            <IconFilterFilled color="white" size={25}/>
-        </div> */}
       </div>
-      <div className="grid grid-cols-4 gap-16 text-white">
+      <div className="grid grid-cols-2 text-white md:grid-cols-4 lg:gap-x-8 xl:gap-x-16 gap-x-2 gap-y-4 lg:gap-y-5">
         {movies.map((movie) => {
           return (
             <div key={movie.id} className="overflow-hidden">
