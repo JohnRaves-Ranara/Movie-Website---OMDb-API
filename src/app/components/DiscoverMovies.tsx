5b43d687073fd988c7cd2ef756eda90905af1c11
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-05-04 16:56:26 +0800

    feat: set proper main tag
    In this commit I:
    
    - In Movies.tsx, replaced fragment with general main tag
    - Also made a main tag for loading skeleton in Movies.tsx
    - Also made a main tag for ErrorComponent
    - Also made a main tag for MovieNotFound
    - removed conditional large spacings in MovieCardsSkeleton since it is already wrapped in a main tag in Movies.tsx,
    just made it a conditional paddding-bottom.

--- src/app/components/DiscoverMovies.tsx
+++ src/app/components/DiscoverMovies.tsx
@@ -25,7 +25,7 @@ export default function DiscoverMovies({
   return (
     <>
       {isFirstPage ? (
-        <main className="min-h-screen bg-gray-950 px-[4vw] lg:px-24 pt-[12vh] sm:pt-[14vh] md:pt-[16vh] lg:pt-[18vh] xl:pt-[16vh] pb-4">
+        <section className="pb-4">
           <FilterDialog allGenres={allGenres}></FilterDialog>
           <header className="flex items-center justify-between gap-8 mb-8">
             <h1 className="text-white text-md mobile-l:text-lg sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl overflow-hidden before:block before:h-8 before:w-[0.45rem] before:bg-purple-500 flex items-center gap-4">
@@ -57,19 +57,17 @@ export default function DiscoverMovies({
               );
             })}
           </section>
-        </main>
+        </section>
       ) : (
-        <main className="min-h-screen bg-gray-950 px-[4vw] lg:px-24 pb-4">
+        <section className="pb-4">
           <section className="grid grid-cols-2 text-white md:grid-cols-4 lg:gap-x-8 xl:gap-x-16 auto-rows-auto gap-x-2 gap-y-4 lg:gap-y-5">
             {movies.map((movie) => {
               return (
-                <div key={movie.id} className="">
-                  <MovieCard movie={movie} allGenres={allGenres} />
-                </div>
+                <MovieCard key={movie.id} movie={movie} allGenres={allGenres} />
               );
             })}
           </section>
-        </main>
+        </section>
       )}
     </>
   );
--- src/app/components/MovieSearchResults.tsx
+++ src/app/components/MovieSearchResults.tsx
@@ -17,7 +17,7 @@ export default function MovieSearchResults({
   allGenres,
 }: MovieSearchResultsProps) {
   return (
-    <main className="min-h-screen bg-gray-950 px-[4vw] lg:px-24 pt-[12vh] sm:pt-[14vh] md:pt-[16vh] lg:pt-[18vh] xl:pt-[16vh] pb-24 ">
+    <main className="pt-[12vh] sm:pt-[14vh] md:pt-[16vh] lg:pt-[18vh] xl:pt-[16vh] min-h-screen bg-gray-950 px-[4vw] lg:px-24 pb-24">
       <header className="flex items-center gap-8 mb-8">
         <h1 className="text-white text-base mobile-l:text-lg sm:text-2xl lg:text-3xl xl:text-4xl overflow-hidden before:block before:h-8 before:w-[0.45rem] before:bg-purple-500 flex items-center gap-4">
           Showing results for "{inputQuery}"
